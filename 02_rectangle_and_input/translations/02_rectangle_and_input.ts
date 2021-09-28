42d57cebf30698e7ded91ed2ea9f556ec926581f
r01and3 <dimka.pronckin@gmail.com>
2021-09-28 20:43:46 +0300

    Some fix

--- 02_rectangle_and_input/translations/02_rectangle_and_input.ts
+++ 02_rectangle_and_input/translations/02_rectangle_and_input.ts
@@ -8,39 +8,4 @@
         <translation type="unfinished"></translation>
     </message>
 </context>
-<context>
-    <name>MainPage</name>
-    <message>
-        <source>Show anchors rectangle</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Show column and row rectangles</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Show grid rectangles</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Show mutation</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Show animation</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Show inputs</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Rectangle and Input application</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Push up to see menu</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
 </TS>
